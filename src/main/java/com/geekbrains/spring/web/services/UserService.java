de9ce4d411bbd68f5ded7f227b69d0c184b9c62d
alekoChe <aleko2117bis@gmail.com>
2022-08-17 22:45:41 +0300

    change ProductsService.java, UserService.java

--- src/main/java/com/geekbrains/spring/web/services/UserService.java
+++ src/main/java/com/geekbrains/spring/web/services/UserService.java
@@ -30,7 +30,7 @@ public class UserService implements UserDetailsService {
     @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = findByUsername(username).orElseThrow(() -> new UsernameNotFoundException(String.format("User '%s' not found", username)));
-        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), mapRolesToAuthorities(user.getRoles()));
+        return new org.springframework.security.core.userdetails.User(user.getUserName(), user.getPassword(), mapRolesToAuthorities(user.getRoles()));
     }
 
     private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles) {
